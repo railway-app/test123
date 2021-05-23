bb162a8c76a20d92939d1e6e7b28cfd3d9ffa0f7
railway-app <84626049+railway-app@users.noreply.github.com>
2021-05-23 11:33:31 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+wget https://storage.googleapis.com/bysh-chef-files/olaris-release/olaris-linux-amd64-v0.3.3.zip
+unzip olaris-linux-amd64-v0.3.3.zip -d ~/olaris
+cd ~/olaris/bin/
+chmod +x olaris
+./olaris
